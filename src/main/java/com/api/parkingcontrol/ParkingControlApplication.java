5cb1c6830d1e68a7d6a24c91b1f3ae799c7f7f5b
ReynaldoHendson <80369346+reynaldo86@users.noreply.github.com>
2023-11-27 19:08:37 -0300

    refact application properties

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -1,10 +1,14 @@
 package com.api.parkingcontrol;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers ={@Server(url = "/", description = "Default Server URL")})
 public class ParkingControlApplication {
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
